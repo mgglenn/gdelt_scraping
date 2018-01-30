c548ec926477680f1f84928dd4352e95fd6312ae
Grace Glenn <mglenngrace@gmail.com>
2018-01-30 15:01:19 -0500

    codecs is a thing... :)

--- gdelt_scrape.py
+++ gdelt_scrape.py
@@ -1,11 +1,16 @@
 import re
 import urllib.request as ur
+import string
+import codecs 
 
 def clean_text(text):
+  cleantext = codecs.getdecoder("unicode_escape")(text)[0]
+
   cleanr = re.compile('<.*?>')
-  cleantext = re.sub(cleanr, '', text)
+  cleantext = re.sub(cleanr, ' ', cleantext)
+  cleantext = ' '.join(cleantext.split('\n'))
   return cleantext
 
 def get_text(link):
     page = ur.urlopen(link)
-    return page.read() 
+    return str(page.read()) 
